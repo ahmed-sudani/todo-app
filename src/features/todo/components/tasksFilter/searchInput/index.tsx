e666ae6f065d46e03492b047aced9271d6730e2a
ahmed-sudani <ahmed.sudani570@gmail.com>
2023-11-24 10:23:26 +0200

    feat(component): search input

---
+++ src/features/todo/components/tasksFilter/searchInput/index.tsx
@@ -0,0 +1,19 @@
+import InputWithSvg from '../../../../../core/components/input/input.with.svg'
+import { JSXInternal } from 'preact/src/jsx'
+import { Signal } from '@preact/signals'
+import { signalSetter } from '../../../../../core/util/signalSetter'
+
+export default function SearchInput({ searchText }: { searchText: Signal<string> }) {
+  const setSearchText = signalSetter(searchText)
+  const onInput: JSXInternal.GenericEventHandler<HTMLInputElement> = (event) => setSearchText(event.currentTarget.value)
+  const inputProps = {
+    img: { src: '/icons/search.svg' },
+    input: {
+      'data-testid': 'feature-todo-filter-tasks-input',
+      id: 'search',
+      onInput,
+      placeholder: 'Search for a task',
+    },
+  }
+  return <InputWithSvg {...inputProps} />
+}
