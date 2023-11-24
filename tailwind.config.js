2bd9575e926b166a0c351ef8a48d4b9005206aa2
ahmed-sudani <ahmed.sudani570@gmail.com>
2023-11-24 09:04:24 +0200

    feat: tailwindcss config file

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+export const mode = 'jit'
+export const purge = ['./src/**/*.{js,jsx,ts,tsx}']
+export const theme = {
+  extend: {},
+}
+export const variants = {
+  extend: {},
+}
+export const plugins = []
