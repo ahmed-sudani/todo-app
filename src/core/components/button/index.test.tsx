a281ae7e29453f001f1232c43ff1324db97035bf
ahmed-sudani <ahmed.sudani570@gmail.com>
2023-11-24 09:38:24 +0200

    feat: core button tests

---
+++ src/core/components/button/index.test.tsx
@@ -0,0 +1,16 @@
+import { describe, expect, it } from 'vitest'
+import Button from '.'
+import { render } from '@testing-library/preact'
+
+describe('Button', () => {
+  it('should render successfully', () => {
+    const text = 'test'
+    const { container } = render(<Button text={text} />)
+    expect(container).toMatchSnapshot()
+  })
+  it('should display text successfully', () => {
+    const text = 'test'
+    const { container } = render(<Button text={text} />)
+    expect(container.textContent).toEqual(text)
+  })
+})
