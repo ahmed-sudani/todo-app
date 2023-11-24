b697bf0095f77d54ef7315779912389ba5128239
ahmed-sudani <ahmed.sudani570@gmail.com>
2023-11-24 10:22:12 +0200

    feat(component): tasks filter

---
+++ src/features/todo/components/tasksFilter/index.tsx
@@ -0,0 +1,14 @@
+import FilterButtons from './buttons'
+import SearchInput from './searchInput'
+import useFilterTasks from '../../hooks/filter'
+
+export default function TaskFilter() {
+  const { tasksToShow, searchText } = useFilterTasks()
+
+  return (
+    <div className="flex items-center gap-5 flex-wrap">
+      <SearchInput searchText={searchText} />
+      <FilterButtons tasksToShow={tasksToShow} />
+    </div>
+  )
+}
