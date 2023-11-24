11d08379883ce4a8889c25bf835326a2429428a6
ahmed-sudani <ahmed.sudani570@gmail.com>
2023-11-24 10:23:09 +0200

    feat(component): tasks table

---
+++ src/features/todo/components/tasksTable/index.tsx
@@ -0,0 +1,12 @@
+import TasksTableBody from './body'
+import TasksTableHeader from './header'
+import { filteredTasks } from '../../signals/tasks'
+
+export default function TasksTable() {
+  return (
+    <table className="text-sm text-left text-gray-500 dark:text-gray-400 border-separate border-spacing-x-0.5 border-spacing-y-1">
+      <TasksTableHeader />
+      <TasksTableBody tasks={filteredTasks} />
+    </table>
+  )
+}
