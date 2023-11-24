8c74706d27d7da22e9ac714bfd33fd8c1b2d8f09
ahmed-sudani <ahmed.sudani570@gmail.com>
2023-11-24 10:24:27 +0200

    feat(component): add TaskRow

---
+++ src/features/todo/components/tasksTable/row/index.tsx
@@ -0,0 +1,36 @@
+import ActionButton from './ActionButton'
+import Button from '../../../../../core/components/button'
+import TaskEditInput from './input'
+import TaskStatus from './status'
+import { TaskType } from '../../../types/task'
+import { deleteTaskById } from '../../../util/tasks'
+import timeSince from '../../../../../core/util/timeSince'
+
+export default function TaskRow({ task }: { task: TaskType }) {
+  const onTaskDelete = () => {
+    deleteTaskById(task.id)
+  }
+
+  return (
+    <tr data-testid={`feature-todo-tasks-table-row-${task.text}`}>
+      <td>
+        <TaskEditInput task={task} />
+      </td>
+      <td className="hidden sm:table-cell">{timeSince(task.createdDate)}</td>
+      <td className="hidden sm:table-cell">
+        <TaskStatus task={task} />
+      </td>
+      <td>
+        <ActionButton task={task} />
+      </td>
+      <td>
+        <Button
+          text="Delete"
+          onClick={onTaskDelete}
+          className="btn-danger"
+          data-testid={`feature-todo-tasks-table-row-btn-delete-${task.text}`}
+        />
+      </td>
+    </tr>
+  )
+}
