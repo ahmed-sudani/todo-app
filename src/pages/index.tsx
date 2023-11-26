130a0b86aa910d78c10539c641e56bff579b444d
ahmed-sudani <ahmed.sudani570@gmail.com>
2023-11-26 07:32:29 +0200

    fix: move Todo to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,21 @@
-import Todo from '../features/todo/components/todo'
+import { completeAllTasks, deleteAllCompletedTasks } from '../util/tasks'
+import Button from '../components/button'
+import Header from '../components/header'
+import TaskFilter from '../features/tasksFilter'
+import TaskForm from '../features/taskForm'
+import TasksTable from '../features/tasksTable'
 
 export default function TodoPage() {
-  return <Todo />
+  return (
+    <div className="flex flex-col gap-4 mx-2 sm:mx-auto mt-5 max-w-4xl p-2 sm:p-6 border border-gray-200 rounded-lg shadow">
+      <Header />
+      <TaskForm />
+      <TaskFilter />
+      <div className="flex gap-1 sm:justify-end justify-start items-center">
+        <Button text="Complete All Tasks" onClick={completeAllTasks} className="btn-primary" />
+        <Button text="Delete Completed Tasks" onClick={deleteAllCompletedTasks} className="btn-danger" />
+      </div>
+      <TasksTable />
+    </div>
+  )
 }
